18285b953d7de3239aa94fbeb3ebbe196dfbefd4
saadbangashh <i200508@nu.edu.pk>
2023-11-28 23:33:04 +0500

    Fixed Error with Workflow File to test github actions

--- src/main/java/com/example/actuatordemo/ActuatorDemoApplication.java
+++ src/main/java/com/example/actuatordemo/ActuatorDemoApplication.java
@@ -8,5 +8,6 @@ public class ActuatorDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActuatorDemoApplication.class, args);
+		System.out.println("Error Fixed!");
 	}
 }
